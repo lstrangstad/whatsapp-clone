c5cab396e7047f07aa2eddeb0e50c725614623e0
Linus <linusstrangstad@gmail.com>
2021-01-24 23:49:42 +0100

    React setup done

---
+++ whatsapp-clone-mern/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD0NMhdwoSg0mxiM_GiAV-bncb2nrGOVMI",
+  authDomain: "whatsapp-mern-54536.firebaseapp.com",
+  projectId: "whatsapp-mern-54536",
+  storageBucket: "whatsapp-mern-54536.appspot.com",
+  messagingSenderId: "940351099973",
+  appId: "1:940351099973:web:4b75b816c62f968d937f2c",
+};
